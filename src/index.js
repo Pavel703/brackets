4dfac5f69d6480eeffd30f8360569d6b08293e88
Pavel703 <43139617+Pavel703@users.noreply.github.com>
2020-03-22 12:07:06 +0200

    completed task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  const brackets = str.split('');
+  for (let i = 0; i < brackets.length; i++){
+    for (let j = 0; j < bracketsConfig.length; j++){
+      if (bracketsConfig[j][0] === brackets[i] && bracketsConfig[j][1] === brackets[i + 1]){
+        brackets.splice(i, 2);
+        i = i - 2;
+      }
+    }
+  }
+    return brackets.length === 0 ? true : false;
+};
